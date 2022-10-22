4f2dd781bab0ef20d647d5f6053d89a0ea009ece
coffee <39930080+coffee522jelly-development@users.noreply.github.com>
2022-10-22 19:24:56 +0900

    Responsive
    Version 0.0.3.3
    
    Responsive Fixed

--- js/common.js
+++ js/common.js
@@ -8,3 +8,26 @@ const inputSlideBarElement = document.getElementById('inputSlideBar');
 inputSlideBarElement.addEventListener('change', function(){
 	fontsize = inputSlideBarElement.value;
 });
+
+// mobile editor
+function getViewportSizeAndAdjust() {
+	const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+	if (w < 414){
+		document.getElementById("EditView").classList.remove("row-cols-2");
+		document.getElementById("EditView").classList.add("row-cols-1");
+	}
+	else{
+		document.getElementById("EditView").classList.remove("row-cols-1");
+		document.getElementById("EditView").classList.add("row-cols-2");
+	}
+  }
+  window.addEventListener('load', (e) => {
+	getViewportSizeAndAdjust();
+  });
+  window.addEventListener('resize', (e) => {
+	getViewportSizeAndAdjust();
+  });
+
+window.onload = getViewportSizeAndAdjust;
